5ec6f5c531726ff106adbb92f898fe535ace8bc9
Piong Zi Yao <zpio0001@student.monash.edu>
2020-06-07 10:21:11 +0800

    Modify GameSetting class to add key for helicopter

--- src/game/GameSetting.java
+++ src/game/GameSetting.java
@@ -1,7 +1,9 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.FancyGroundFactory;
@@ -15,6 +17,8 @@ public class GameSetting {
 	private GameMap compound;
 	private GameMap town;
 	private FancyGroundFactory groundFactory;
+	private Helipad helipad;
+	private ArrayList<PortableItem> keyList = new ArrayList<>();
 	private Player player;
 	private Location compoundVehicleLocation;
 	private Location townVehicleLocation;
@@ -81,7 +85,7 @@ public class GameSetting {
 	}
 
 	
-	public void setUpHumanAndZombie() {
+	public void setUpCompoundHuman() {
 		String[] humans = {"Carlton", "May", "Vicente", "Andrea", "Wendy",
 				"Elina", "Jaquelyn"};
 		int x, y;
@@ -102,14 +106,46 @@ public class GameSetting {
 			}
 			while (compound.at(x, y).containsAnActor());
 			compound.at(x,  y).addActor(new Farmer(name));
-		}
+		}	
+	}
+	
+	public void setUpCompoundZombie() {
+		Zombie zombie1 = new Zombie("Groan");
+		Zombie zombie2 = new Zombie("Boo");
+		Zombie zombie3 = new Zombie("Uuuurgh");
+		Zombie zombie4 = new Zombie("Mortalis");
+		Zombie zombie5 = new Zombie("Gaaaah");
+		Zombie zombie6 = new Zombie("Aaargh");
+		Zombie zombie7 = new Zombie("Baaahhh");
+		Zombie zombie8 = new Zombie("Yeeeeet");
 		
-		compound.at(30, 20).addActor(new Zombie("Groan"));
-		compound.at(30,  18).addActor(new Zombie("Boo"));
-		compound.at(10,  4).addActor(new Zombie("Uuuurgh"));
-		compound.at(50, 18).addActor(new Zombie("Mortalis"));
-		compound.at(1, 10).addActor(new Zombie("Gaaaah"));
-		compound.at(62, 12).addActor(new Zombie("Aaargh"));
+		compound.at(30, 20).addActor(zombie1);
+		compound.at(30,  18).addActor(zombie2);
+		compound.at(10,  4).addActor(zombie3);
+		compound.at(50, 18).addActor(zombie4);
+		compound.at(1, 10).addActor(zombie5);
+		compound.at(62, 12).addActor(zombie6);
+		compound.at(70, 5).addActor(zombie7);
+		compound.at(5, 6).addActor(zombie8);
+		
+		List<Zombie> zombieList = new ArrayList<>();
+		zombieList.add(zombie1);
+		zombieList.add(zombie2);
+		zombieList.add(zombie3);
+		zombieList.add(zombie4);
+		zombieList.add(zombie5);
+		zombieList.add(zombie6);
+		zombieList.add(zombie7);
+		zombieList.add(zombie8);
+		
+		Random rand = new Random();
+		int numberOfKeys = keyList.size();
+		
+		for (int i = 0; i < numberOfKeys; i++) {
+			Zombie zombie = zombieList.get(rand.nextInt(zombieList.size()));
+			zombie.addItemToInventory(keyList.get(i));
+			zombieList.remove(zombie);
+		}
 	}
 	
 	
@@ -117,25 +153,31 @@ public class GameSetting {
 		compoundVehicleLocation = compound.at(42, 13);
 		townVehicleLocation = town.at(1, 1);
 		
-		Helipad helipad = new Helipad(player);
-		PortableItem key1 = new PortableItem("key1", 'k');
-		helipad.addKey(key1);
 		Helicopter compoundVehicle = new Helicopter("Helicopter", '^');
 		Helicopter townVehicle = new Helicopter("Helicopter", '^');
 		compoundVehicle.setDestination(townVehicleLocation, "to town");
 		townVehicle.setDestination(compoundVehicleLocation, "to compound");
 		
-		compound.at(compoundVehicleLocation.x(), compoundVehicleLocation.y()).setGround(helipad);
-		compound.at(compoundVehicleLocation.x(), compoundVehicleLocation.y()).addItem(compoundVehicle);
-		town.at(townVehicleLocation.x(), townVehicleLocation.y()).setGround(helipad);
-		town.at(townVehicleLocation.x(), townVehicleLocation.y()).addItem(townVehicle);
+		compoundVehicleLocation.setGround(helipad);
+		compoundVehicleLocation.addItem(compoundVehicle);
+		townVehicleLocation.setGround(helipad);
+		townVehicleLocation.addItem(townVehicle);
+		
+		PortableItem key1 = new PortableItem("key1", 'k');
+		PortableItem key2 = new PortableItem("key2", 'k');
+		keyList.add(key1);
+		keyList.add(key2);
+		helipad.addKey(key1);
+		helipad.addKey(key2);
 		
 //		player.addItemToInventory(key1);	
 	}
 	
+	
+	
 
 	public World setUpGame() {
-		setUpHumanAndZombie();
+		setUpCompoundHuman();
 		setUpVehicles();
 		return world;
 	}
