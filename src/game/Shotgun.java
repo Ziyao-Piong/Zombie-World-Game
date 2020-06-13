d1bc6d5b1823c3c30868947bcf360b908450ead4
Heng Yi Kin <yhen0006@student.monash.edu>
2020-06-13 23:45:09 +0800

    Heng Yi Kin - Added and modified the Java Documentation of Ammo, AmmunitionBag, PickUpAmmoAction, Shotgun, ShotgunAmmo, Sniper and SniperAmmo classes.

--- src/game/Shotgun.java
+++ src/game/Shotgun.java
@@ -1,18 +1,27 @@
 package game;
 
+import edu.monash.fit2099.engine.Action;
+
+import java.util.List;
+
+/**
+ * An class representing Shotgun that extends Super class Guns which is also a type of weapon.
+ *
+ * @author Yi Kin Heng
+ */
 public class Shotgun extends Gun {
     private static ShotgunAmmo shotgunAmmo;
 
     /**
-     * Constructor.
+     * Constructor that initialises the instance variables name, displayChar , damage and the verb of the Shotgun.
      */
     public Shotgun() {
-        super("Shotgun", 's', 40, "blasts", shotgunAmmo);
+        super("Shotgun", 's', 20, "blasts", shotgunAmmo);
     }
 
 
-//    @Override
-//    public boolean trigger() {
-//        return shotgunAmmo.getAmmoCount() > 0;
-//    }
+    @Override
+    public List<Action> getAllowableActions() {
+        return super.getAllowableActions();
+    }
 }
--- src/game/ShotgunAmmo.java
+++ src/game/ShotgunAmmo.java
@@ -1,7 +1,7 @@
 package game;
 
 /**
- * A class for ShotGunAmmo which is a type of Ammo that is required to used 
+ * A class for ShotGunAmmo which is a type of Ammo that is required to used
  * Shotgun weapon.
  *
  * @author Yi Kin Heng
@@ -11,7 +11,8 @@ public class ShotgunAmmo extends Ammo {
     /***
      * Constructor that calls Super class's constructor to initialise the 
      * name and displayChar of the ShotgunAmmo item.
-     * @param ammunitionBag	The ammunition bag of the player
+     * This item can also be picked up by player.
+     * @param ammunitionBag    The ammunition bag of the player
      */
     public ShotgunAmmo(AmmunitionBag ammunitionBag) {
         super("Shotgun Ammo", '*');
